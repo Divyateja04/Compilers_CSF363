5de04a6c3d336918a2deb4a591da611e59227960
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-04-20 11:23:59 +0530

    feat: all pass except todo

--- Assignment2/Task2/all.py
+++ Assignment2/Task2/all.py
@@ -8,13 +8,13 @@ for i in range(COUNT):
     os.system('echo "======================================="')
     os.system(f'echo "Test Case {i+1}"')
     
-    with open(f'./testcases/{i + 1}.txt', 'r') as f:
-      test_case = f.read()
-      print(test_case)
-                
+    os.system(f'cat ./testcases/{i + 1}.txt > sample.txt')
+    os.system(f'cat sample.txt')
+    
     os.system(f'echo "Deleting old files"')
     os.system(f'rm -f ADKM3773.out')
     os.system(f'rm -f y.tab.c')
+    os.system(f'rm -f y.tab.h')
     os.system(f'rm -f lex.yy.c')
     os.system(f'echo "Compiling yacc program"')
     os.system(f'yacc -d ADKM3773.y -Wconflicts-rr -Wconflicts-sr -Wcounterexamples')
@@ -23,4 +23,10 @@ for i in range(COUNT):
     os.system(f'echo "Compiling the program in gcc"')
     os.system(f'gcc y.tab.c lex.yy.c -ll -o ADKM3773.out')
     os.system(f'./ADKM3773.out')
+
+os.system(f'echo ""')
+os.system(f'rm -f ADKM3773.out')
+os.system(f'rm -f y.tab.c')
+os.system(f'rm -f y.tab.h')
+os.system(f'rm -f lex.yy.c')
     
