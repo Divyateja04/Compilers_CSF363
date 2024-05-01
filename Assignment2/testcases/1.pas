006a63330e0781df15dfc9c02d143d31bdfe93a2
Saphereye <adarshdas950@gmail.com>
2024-05-02 02:13:31 +0530

    chore: changed 1.pas for parity

--- Assignment2/testcases/1.pas
+++ Assignment2/testcases/1.pas
@@ -1,38 +1,67 @@
 program ArrayAverage;
 var
-  numbers: array[1..5] of char; // TODO: size matters
-  i, a, b: Integer;
+  numbers: array[1..5] of Integer;
+  i, a, b, d, sum, digit, number, count : Integer;
   average : real;
+  x: boolean;
   c: char;
 begin
-  i := 4;
-  i := i + 1;
-  average := 0.5;
+  i := 100;
+  i := i + i * (i - i) + (i - i) * d;
+  x := (7 < 5) and (6 > 5);
+  a := -3;
+  count := 0;
+  number := 0;
+  digit := 0.0;
+
+  numbers[3] := 4;
+
+  write("Enter 5 integer values:");
+
   c := 'c';
+  write(c);
 
-  numbers[1] := c;
-  numbers[2] := 5;
+  for i := 1 to 5 do
+  begin
+    read(numbers[i]);
+  end;
 
-  writeln("Test output: ", i, " ", i, " ", average, " ", c);
-  writeln(numbers[1], " ", numbers[2]);
-  // writeln("Test output: ", i, " ", i, " ", average, " ", c);
-  // // read(a);
-  // writeln("Input: ", a);
+  for i := 1 to 5 do
+  begin
+    write(numbers[i]);
+  end;
 
-  // a := 0;
-  // b := 0;
-  // for i := 1 to 5 do
-  // begin
-  //   a := a + 1;
-  //   b := b + a;
-  // end;
-  // writeln("Sum: ", b);
+  sum := 0;
+  for i := 1 to 5 do
+  begin
+    sum := sum + 6 * 7 + numbers[i];
+  end;
 
-  // a := 5;
+  if x then
+  begin
+    a := numbers[i] + 9;
+    write("x is true");
+  end
+  else
+  begin
+    write("x is false");
+    a := numbers[i] - 9;
+  end;
 
-  // while a > 0 do
-  // begin
-  //   a := a - 1;
-  //   writeln("a: ", a);
-  // end;
+
+  while number <> 0 do
+  begin
+    digit := number - (number / 10) * 10;
+    
+    if digit <> 0 then
+    begin
+      count:=count+1;
+    end;
+
+    number := number / 10;
+  end;
+
+  average := sum / 5;
+  write("The sum and average are: "); 
+  write(sum, average);
 end.
