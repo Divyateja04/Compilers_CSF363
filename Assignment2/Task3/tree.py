a8a52221430b9f616384c38662f8eff1d1c28dc5
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-05-02 10:15:47 +0530

    chore: make the tree print to file

--- Assignment2/Task3/tree.py
+++ Assignment2/Task3/tree.py
@@ -2,19 +2,17 @@ from nltk.tree import *
 
 # assign your output (generalied list of the syntax tree) to varaible text
 f = open('syntaxtree.txt', 'r')
+output = open('syntaxtree_output.txt', 'w')
 
 text = f.readlines()[0]
 f.close()
-
-
-
 
 text = text.replace("(", "ob")    #in the syntax tree, 'ob' will display in place of '('
 text = text.replace(")", "cb")    #in the syntax tree, 'cb' will display in place of ')'
 text = text.replace("[", "(")
 text = text.replace("]", ")")
 
-
 tree = Tree.fromstring(text)
-tree.pretty_print(unicodelines=True, nodedist=2)   
+# print the tree to the output file
+tree.pretty_print(nodedist=2, stream=output)   
 
